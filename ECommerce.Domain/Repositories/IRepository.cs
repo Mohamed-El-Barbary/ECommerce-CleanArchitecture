0231b113ab0c7fe7d1adc2bb7462ed0bd18679c1
Mohamed-El-Barbary <mohamedelbarbary511@gmail.com>
2026-08-06 03:55:02 +0300

    feat(infrastructure): add generic repository interface and implementation

---
+++ ECommerce.Domain/Repositories/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using ECommerce.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerce.Domain.Repositories
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        Task<T?> GetByIdAsync(Guid id, CancellationToken ck = default);
+
+        Task<IReadOnlyList<T>> GetAllAsync(CancellationToken ck = default);
+
+        void Add(T entity);
+
+        void Update(T entity);
+
+        void Delete(T entity);
+
+    }
+}
