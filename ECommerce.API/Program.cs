086ec35dba27325b501beb8c5979acd5dc8cd024
Mohamed-El-Barbary <mohamedelbarbary511@gmail.com>
2026-08-06 21:02:41 +0300

    feat(infrastructure): add DI configuration for seeding and DB migrate

--- ECommerce.API/Program.cs
+++ ECommerce.API/Program.cs
@@ -1,21 +1,28 @@
 using ECommerce.API;
 using ECommerce.Infrastructure;
+using ECommerce.Infrastructure.Persistence.Data.DbContexts;
 using ECommerce.Infrastructure.Persistence.Seeding;
 using ECommerce.UseCases;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Add services to the container.
-builder.Services.AddPresentation(); 
-builder.Services.AddInfrastructure(builder.Configuration); 
-builder.Services.AddApplication(); 
+builder.Services.AddPresentation();
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddApplication();
 
 var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    await using var scope = app.Services.CreateAsyncScope();
+    var dbSeed = scope.ServiceProvider.GetRequiredService<DatabaseSeeder>();
+    var dbContext = scope.ServiceProvider.GetRequiredService<StoreDbContext>();
 
-await using var scope = app.Services.CreateAsyncScope();
-var dbSeed = scope.ServiceProvider.GetRequiredService<DatabaseSeeder>();
-await dbSeed.SeedAll();
+    await dbContext.Database.MigrateAsync();
+    await dbSeed.SeedAll();
+}
 // Configure the HTTP request pipeline.
 
 app.Run();
