074e095b67ec5e9be02a30031e81ecaa58c1f037
Mohamed-El-Barbary <mohamedelbarbary511@gmail.com>
2026-09-21 22:56:04 +0300

    feat(Infrastructure): implement specification interface

---
+++ ECommerce.Domain/Specifications/IncludeExpressionInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace ECommerce.Domain.Specifications;
+
+public sealed record IncludeExpressionInfo(
+    LambdaExpression LambdaExpression,
+    LambdaExpression PreviousExpression
+    );
